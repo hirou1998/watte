29197573b59aeab31af69caacf5807e1d046b8a8
hirou1998 <tanahi1025@gmail.com>
2021-02-25 03:07:33 +0900

    amountControllerの認証を一度削除

--- app/Http/Controllers/Api/AmountController.php
+++ app/Http/Controllers/Api/AmountController.php
@@ -10,20 +10,28 @@ use App\Models\Amount;
 class AmountController extends Controller
 {
     public function lists(Event $event, Amount $amount, Request $request)
-    {        
-        $userToken = $request->bearerToken();
+    {  
+        $amount_lists = $event
+            ->amounts()
+            ->with('line_friend')
+            ->orderBy('archive_flg', 'asc')
+            ->orderBy('created_at', 'desc')
+            ->with('deals')
+            ->get();
+        return $amount_lists;      
+        // $userToken = $request->bearerToken();
 
-        if($userToken && $userToken == session()->get('line_id')){
-            $amount_lists = $event
-                ->amounts()
-                ->with('line_friend')
-                ->orderBy('archive_flg', 'asc')
-                ->orderBy('created_at', 'desc')
-                ->with('deals')
-                ->get();
-            return $amount_lists;
-        }else{
-            abort(401, 'Unauthorized');
-        }
+        // if($userToken && $userToken == session()->get('line_id')){
+        //     $amount_lists = $event
+        //         ->amounts()
+        //         ->with('line_friend')
+        //         ->orderBy('archive_flg', 'asc')
+        //         ->orderBy('created_at', 'desc')
+        //         ->with('deals')
+        //         ->get();
+        //     return $amount_lists;
+        // }else{
+        //     abort(401, 'Unauthorized');
+        // }
     }
 }
